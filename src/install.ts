0dc073bacc41c60e96adbe003ca39bb5d820e640
Cody Casterline <cody.casterline@gmail.com>
2025-07-10 22:34:51 -0700

    installer fixes

--- src/install.ts
+++ src/install.ts
@@ -9,10 +9,17 @@ const fullCommand = [
     // IIRC Dax & ts-cmd use `env` permissions for configuring terminal output.
     "-RWEN",
 
+    // If a user runs this from a deno project dir, they'll get a warning about a local config.
+    // We don't want to use the local config anwyay, we want to use the config in JSR.
+    "--no-config",
+
+    // Always grab the latest version.
+    "--reload=jsr:@nfnitloop/gemi",
+
     "jsr:@nfnitloop/gemi"
 ]
 
-console.log(`We will run this command to install the app:`)
+console.log(`We will run this command to install the gemi cli:`)
 console.log(JSON.stringify(fullCommand))
 console.log()
 
